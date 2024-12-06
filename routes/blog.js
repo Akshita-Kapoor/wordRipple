ec4e7e5d7efebf52425aee5eadb19633086dde9f
Akshita <ak.akshita21@gmail.com>
2024-12-05 18:10:54 -0700

    add/show comments for blog

--- routes/blog.js
+++ routes/blog.js
@@ -6,17 +6,28 @@ const {
     upload
 } = require("../controllers/blog");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const router = Router();
 
 router.get("/add-new", showAddNewBlogPage);
 router.get("/:id", async (req, res) => {
-    const blog = await Blog.findById(req.params.id);
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({blogId: req.params.id}).populate("createdBy")
     return res.render('blog', {
         user: req.user,
-        blog
+        blog,
+        comments
     })
 })
 router.post("/", upload.single('coverImage'), handleAddNewBlogPage);
 
+router.post("/comment/:blogId", async(req, res) => {
+    await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id
+    })
+    return res.redirect(`/blog/${req.params.blogId}`);
+})
 module.exports = router;
